25a5ebe2ebd1e23937a89b7dcc27adbed7a61343
Alberto Soutullo <albertorendo@status.im>
2026-02-24 15:40:51 +0100

    Add functions to discover service and connect to bootstrap node

--- nim-test-node/kad-dht/main.nim
+++ nim-test-node/kad-dht/main.nim
@@ -51,6 +51,55 @@ proc runWarmup(kad: KadDHT, selfId: PeerId) {.async.} =
 
   notice "Warmup complete"
 
+proc resolveAddress(muxer: string, tAddress: string): Future[Result[seq[MultiAddress], string]] {.async.} =
+  while true:
+    try:
+      let resolvedAddrs =
+        if muxer.toLowerAscii() == "quic":
+          let quicV1 = MultiAddress.init("/quic-v1").tryGet()
+          resolveTAddress(tAddress).mapIt(
+            MultiAddress.init(it, IPPROTO_UDP).tryGet()
+              .concat(quicV1).tryGet()
+          )
+        else:
+          resolveTAddress(tAddress).mapIt(MultiAddress.init(it).tryGet())
+      info "Address resolved", tAddress = tAddress, resolvedAddrs = resolvedAddrs
+      return ok(resolvedAddrs)
+    except CatchableError as exc:
+      warn "Failed to resolve address", address = tAddress, error = exc.msg
+      await sleepAsync(15.seconds)
+
+proc resolveService(muxer: string, service: string): Future[Result[seq[MultiAddress], string]] {.async.} =
+  # If the service doesn't contain a port, append the default one
+  let tAddress = if ":" in service: service else: service & ":" & $myPort
+
+  # Call the existing resolveAddress helper
+  let resolvedAddrs = (await resolveAddress(muxer, tAddress)).valueOr:
+    return err("Failed to resolve address " & tAddress & ": " & error)
+
+  var addrs = resolvedAddrs
+  if addrs.len > 0:
+    let rng = newRng()
+    rng.shuffle(addrs)
+
+  return ok(addrs)
+
+
+proc connectToBootstraps(switch: Switch, muxer: string, service: string) {.async.} =
+  let addrsRes = await resolveService(muxer, service)
+  let addrs = addrsRes.valueOr:
+    error "Failed to resolve bootstrap service", service = service, error = error
+    return
+
+  for addr in addrs:
+    try:
+      discard await switch.connect(addr, allowUnknownPeerId=true)
+      notice "Connected to bootstrap", address = addr
+      await sleepAsync(2.seconds)
+    except CatchableError as exc:
+      warn "Failed to connect to bootstrap", address = addr, error = exc.msg
+
+
 # proc runProbe(kad: KadDHT) {.async.} =
 #   notice "Starting probe loop"
 #   while true:
